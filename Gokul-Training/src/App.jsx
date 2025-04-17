3faa6305cb1cc62b2d5eee03c1e19220924eb162
LivinkumarS <001liv100@ggmail.com>
2025-04-17 15:02:51 +0530

    State, UseState, OnClick

--- Gokul-Training/src/App.jsx
+++ Gokul-Training/src/App.jsx
@@ -1,11 +1,22 @@
-import "./App.css";
-import Card from "./card";
+import React, { useState } from "react";
 
-function App() {
-  return <div>
-    <Card Image="https://m.media-amazon.com/images/M/MV5BYTQ3NDU3YjEtMDQxNi00N2QxLWIxNjAtMmFlOWM5ZDY3MGVkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" Name="Vijay" Age="51" Role="Hero" NoOfMovies="68"/>
-    <Card Image="https://images.ottplay.com/webstories/wp-content/uploads/2024/07/ajith-kumar-lesser-known-facts-.png" Name="Ajith" Age="52" Role="Hero" NoOfMovies="65"/>
-  </div>;
+export default function App() {
+  const [name, setName] = useState("Hari"); //create
+
+  function changeName(){
+    setName((prev)=>{
+      return prev+" kumar"
+    })
+  }
+
+  return (
+    <div>
+      <h1>The name is {name}</h1>
+      <button
+        onClick={()=>{changeName()}}
+      >
+        Clickme
+      </button>
+    </div>
+  );
 }
-
-export default App;
