ca4e431e538f5fcb8a5d3af74e1f13c3a0980d96
LivinkumarS <001liv100@ggmail.com>
2025-04-22 12:30:46 +0530

    List rendering

--- Gokul-Training/src/App.jsx
+++ Gokul-Training/src/App.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
+import Name from "./Name";
 
 export default function App() {
-  const [doLove, setDoLove] = useState(true);
+  // const movieList = ["Theri", "Kaththi", "Sachin", "Ghillie", "Mersal"];
+
+  const nameList = [
+    { id: 0, name: "Rahul" },
+    { id: 1, name: "Gokul" },
+    { id: 2, name: "Tharick" },
+    { id: 3, name: "Trisha" },
+    { id: 4, name: "Andriya" },
+    { id: 5, name: "Thamanna" },
+    { id: 6, name: "Nayanthara" },
+  ];
 
   return (
     <div>
-      {doLove && <h1>I would love to code</h1>}
-      <button
-        onClick={() => {
-          setDoLove((prev)=>{return !prev});
-        }}
-      >
-        Click me!
-      </button>
+      <h2>Name List</h2>
+
+      <ul>
+        {nameList.map((ele, ind) => {
+          return <Name key={ind} id={ele.id} name={ele.name} />;
+        })}
+      </ul>
     </div>
   );
 }
---
+++ Gokul-Training/src/Name.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Name(prop) {
+  return (
+    <li>
+      Id: {prop.id} Name: {prop.name}
+    </li>
+  );
+}
