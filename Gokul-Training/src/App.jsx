b7e3008024552e397297793b43193cc7acb8e599
LivinkumarS <001liv100@ggmail.com>
2025-04-22 12:42:31 +0530

    List Rendering Using Map

--- Gokul-Training/src/App.jsx
+++ Gokul-Training/src/App.jsx
@@ -1,28 +1,32 @@
 import React, { useState } from "react";
-import Name from "./Name";
 
 export default function App() {
-  // const movieList = ["Theri", "Kaththi", "Sachin", "Ghillie", "Mersal"];
-
-  const nameList = [
-    { id: 0, name: "Rahul" },
-    { id: 1, name: "Gokul" },
-    { id: 2, name: "Tharick" },
-    { id: 3, name: "Trisha" },
-    { id: 4, name: "Andriya" },
-    { id: 5, name: "Thamanna" },
-    { id: 6, name: "Nayanthara" },
-  ];
+  const [movieList, setMovieList] = useState([
+    "Theri",
+    "Kaththi",
+    "Sachin",
+    "Ghillie",
+    "Mersal",
+  ]);
 
   return (
     <div>
-      <h2>Name List</h2>
+      {movieList.map((ele, ind) => {
+        return <h1 key={ind}>{ele}</h1>;
+      })}
 
-      <ul>
-        {nameList.map((ele, ind) => {
-          return <Name key={ind} id={ele.id} name={ele.name} />;
-        })}
-      </ul>
+      <button
+        onClick={() => {
+          setMovieList((prev) => {
+            let temp = prev;
+            temp.pop();
+
+            return [...temp];
+          });
+        }}
+      >
+        clear
+      </button>
     </div>
   );
 }
--- Gokul-Training/src/Name.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Name(prop) {
-  return (
-    <li>
-      Id: {prop.id} Name: {prop.name}
-    </li>
-  );
-}
