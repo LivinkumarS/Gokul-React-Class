91a79ce2082c5c819dfeeb1fc648bf4de6800ac6
LivinkumarS <001liv100@ggmail.com>
2025-05-13 12:27:58 +0530

    mennu sec update

--- final-project/src/components/MenuSection.jsx
+++ final-project/src/components/MenuSection.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaAngleLeft } from "react-icons/fa";
 
-
 export default function MenuSection() {
-  return <div className="menu-sec">
-    {/* Arrow  */}
-    <div className="arrow-btn">
-      <FaAngleLeft/>
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div style={{position: "relative"}}>
+      <div
+        className="menu-sec"
+        style={isOpen ? {} : { width: "0", padding: "0" }}
+      >
+        <input type="text" placeholder="Add Text" name="" id="" />
+
+        {/* Arrow  */}
+      </div>
+      <div className="arrow-btn" onClick={() => setIsOpen(!isOpen)}>
+        <FaAngleLeft style={isOpen ? {} : { transform: "rotate(180deg)" }} />
+      </div>
     </div>
-  </div>;
+  );
 }
